6cd5579c43e07c06e70285487d08b4c43ffab62d
Chaks <darimireddy@googlemail.com>
2013-07-13 20:17:36 +0530

    Updated

--- jax-rpc_client/src/com/tcs/demo/JAXRPCStaticStubClient.java
+++ jax-rpc_client/src/com/tcs/demo/JAXRPCStaticStubClient.java
@@ -7,6 +7,8 @@ package com.tcs.demo;
 import com.tcs.demo.service.GlobalWeatherSoap;
 import com.tcs.demo.service.GlobalWeatherSoap_Stub;
 import com.tcs.demo.service.GlobalWeather_Impl;
+import java.rmi.RemoteException;
+import javax.xml.rpc.ServiceException;
 import javax.xml.rpc.Stub;
 
 /**
@@ -18,33 +20,21 @@ public class JAXRPCStaticStubClient {
   /**
    * @param args the command line arguments
    */
-  public static void main(String[] args) {
-	// TODO code application logic here
+  public static void main(String[] args) throws RemoteException, ServiceException {
 
-	try {
-	  // This code block invokes the GlobalWeatherSoap:getWeather operation on web service
-	  com.tcs.demo.service.GlobalWeather globalWeather = new com.tcs.demo.service.GlobalWeather_Impl();
-	  com.tcs.demo.service.GlobalWeatherSoap globalWeatherSoap = globalWeather.getGlobalWeatherSoap();
+    //This code block invokes the GlobalWeatherSoap:getWeather operation on web service
+    //http://www.webservicex.net/globalweather.asmx?WSDL
+    com.tcs.demo.service.GlobalWeather globalWeather = new com.tcs.demo.service.GlobalWeather_Impl();
+    com.tcs.demo.service.GlobalWeatherSoap globalWeatherSoap = globalWeather.getGlobalWeatherSoap();
 
-	  System.out.println("getCitiesByCountry(): " + globalWeatherSoap.getCitiesByCountry("India"));
-	  System.out.println("globalWeatherSoap(): " + globalWeatherSoap.getWeather("Bombay", "India"));
+    System.out.println("getCitiesByCountry(): " + globalWeatherSoap.getCitiesByCountry("India"));
+    System.out.println("globalWeatherSoap(): " + globalWeatherSoap.getWeather("Bombay", "India"));
 
-	  //Overriding the endpoint
-	  GlobalWeatherSoap_Stub globalWeatherSoap_Stub =
-			  (GlobalWeatherSoap_Stub) new GlobalWeather_Impl().getPort(GlobalWeatherSoap.class);
-	  globalWeatherSoap_Stub._setProperty(
-			  Stub.ENDPOINT_ADDRESS_PROPERTY, "http://localhost:6789/globalweather.asmx");
-	  System.out.println("getCitiesByCountry(): " + globalWeatherSoap_Stub.getCitiesByCountry("India"));
-	} catch (javax.xml.rpc.ServiceException ex) {
-	  java.util.logging.Logger.getLogger(
-			  com.tcs.demo.service.GlobalWeather.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-	} catch (java.rmi.RemoteException ex) {
-	  java.util.logging.Logger.getLogger(
-			  com.tcs.demo.service.GlobalWeather.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-	} catch (Exception ex) {
-	  java.util.logging.Logger.getLogger(
-			  com.tcs.demo.service.GlobalWeather.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-	}
-
+    //Overriding the endpoint
+    GlobalWeatherSoap_Stub globalWeatherSoap_Stub =
+            (GlobalWeatherSoap_Stub) new GlobalWeather_Impl().getPort(GlobalWeatherSoap.class);
+    globalWeatherSoap_Stub._setProperty(
+            Stub.ENDPOINT_ADDRESS_PROPERTY, "http://localhost:6789/globalweather.asmx");
+    System.out.println("getCitiesByCountry(): " + globalWeatherSoap_Stub.getCitiesByCountry("India"));
   }
 }
